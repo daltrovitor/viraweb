edd3e79475cdf12edae676fc7d96e689c8c980a5
narug1fps <narug1fps@gmail.com>
2026-04-13 21:46:04 -0300

    seo

--- app/template.tsx
+++ app/template.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { m } from "framer-motion";
-
-export default function Template({ children } : any )  {
-  return (
-    <m.div
-      initial={{ y: -200, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 1 }}
-    >
-      {children}
-    </m.div>
-  );
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
